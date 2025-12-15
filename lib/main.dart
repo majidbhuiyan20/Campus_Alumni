569416fe7ba25a7feca71b9dd9f792b0deb7107e
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-12-16 01:32:12 +0600

    init firebase campus alumni

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/route/route_manager.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp( ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,18 +26,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(375, 812),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_,context) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Riverpod MVVM Demo',
-            theme: ThemeData(primarySwatch: Colors.blue),
-            onGenerateRoute: RouteGenerator.getRoute,
-            initialRoute: Routes.splashRoute,
-          );
-        }
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Riverpod MVVM Demo',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.splashRoute,
+        );
+      },
     );
   }
 }
